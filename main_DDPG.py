2366856a658b47777d1721a2ba2b67f207529e14
Sebastian Jaimungal <33582519+sebjai@users.noreply.github.com>
2024-01-16 21:03:17 -0500

    the case of N=4

--- main_DDPG.py
+++ main_DDPG.py
@@ -14,12 +14,12 @@ import DDPG, offset_env
 # =============================================================================
 #%%
 env = offset_env.offset_env(T=1/12, sigma=0.5, kappa=0.03, eta = 0.05, xi=0.1,
-                 c=0.25, S0=2.5, R=5, pen=2.5, N = 101)
+                 c=0.25, S0=2.5, R=5, pen=2.5, N = 4)
 
 ddpg = DDPG.DDPG(env,
             gamma = 0.999, 
-            lr=5e-4,
+            lr=0.005,
             name="test" )
  
 #%%    
-ddpg.train(n_iter = 10000, n_plot=1000, n_iter_Q=5, n_iter_pi=1)
+ddpg.train(n_iter = 10_000, n_plot=100, n_iter_Q=5, n_iter_pi=1)
